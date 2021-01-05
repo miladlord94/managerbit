f56c80bdf0ff88e5e38ffc08a7f6abee6f1cb507
miladlord94 <ghasemimilad16@gmail.com>
2021-01-05 20:27:53 +0330

    milad

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>  aaaweeeerrrrrr </h1>
+    <a href="asaas">asaddadasas</a>
+ <!-- //   <canvas id="chart" width="400" height="100" aria-label="Hello ARIA World" role="img"></canvas> -->
+</body>
+
+</html>
